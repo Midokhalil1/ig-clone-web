2c41dc6d21222b77630909031c3584564b372262
Midokhalil1 <midokhalil22@gmail.com>
2022-11-17 14:39:06 -0500

    updated

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,1 +1,20 @@
-export default function Post()
+
+
+import { Card, Avatar } from 'antd'
+
+export default function Post({ post }) {
+  return (
+    <Card
+      style={{ width: 300 }}
+      cover={
+        <img alt={post.description} src={post.photo} />
+      }
+    >
+      <Card.Meta
+        avatar={<Avatar src="https://randomuser.me/api/portraits/women/7.jpg" />}
+        title={post.username}
+        description={post.description}
+      />
+    </Card>
+  )
+}
